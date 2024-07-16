1123ac74f24b1247608061225549931cd9b6e868
dirmane <marcelikosmazaborowski@gmail.com>
2024-07-16 14:54:46 +0200

    Setup

--- Setup.ps1
+++ Setup.ps1
@@ -1,22 +1,25 @@
 # Step 3: Run the setup script
 Write-Host "Running setup script..."
 npm install
+cls
 node setup.js
+cls
 
 # Step 4: Install backend dependencies
 Write-Host "Installing backend dependencies..."
 cd backend
 npm install
+cls
 
 # Step 5: Install frontend dependencies
 Write-Host "Installing frontend dependencies..."
 cd ../automail
 npm install
+cls
 
 # Step 6: Start the backend and frontend
 Write-Host "Starting backend and frontend..."
 cd ..
-start cmd /c "cd backend && npm start"
-start cmd /c "cd automail && npm start"
-
+start powershell {cd backend; npm start}
+start powershell {cd automail; npm start}
 Write-Host "Setup complete. The backend and frontend are running."
